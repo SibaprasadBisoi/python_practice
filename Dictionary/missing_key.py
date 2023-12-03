b55edc209aa2594612e743cc398aaf40b488cb55
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2023-12-03 22:54:01 +0530

    Added file for find the missing key in dictionary

---
+++ Dictionary/missing_key.py
@@ -0,0 +1,10 @@
+dict = {'a': 1, 'b': 2, 'c':3}
+if 'e' in dict:
+    print(dict['e'])
+else:
+    print("Key not found")
+if 'a' in dict:
+    print(dict['a'])
+else:
+    print("Key not found")
+print(dict)
