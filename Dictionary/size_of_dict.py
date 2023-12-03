aa213005e2e65ffeee75134adc5b5e33d17794d1
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2023-12-03 23:28:23 +0530

    Added file for getting the size of dictionary

---
+++ Dictionary/size_of_dict.py
@@ -0,0 +1,10 @@
+import sys
+ 
+# sample Dictionaries
+dic1 = {"A": 1, "B": 2, "C": 3}
+dic2 = {"Geek1": "Raju", "Geek2": "Nikhil", "Geek3": "Deepanshu"}
+dic3 = {1: "Lion", 2: "Tiger", 3: "Fox", 4: "Wolf"}
+
+print("size of dict1 is: " + str(sys.getsizeof(dic1)) + "bytes")
+print("size of dict2 is: " + str(sys.getsizeof(dic2)) + "bytes")
+print("size of dict3 is: " + str(sys.getsizeof(dic3)) + "bytes")
