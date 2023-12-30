d5fca7d350568b416dde5a19c467ccd7330b524f
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2023-12-30 22:15:26 +0530

    Updated file for finding keys and values

--- Dictionary/find_key_value.py
+++ Dictionary/find_key_value.py
@@ -3,3 +3,9 @@ dict = {"rajvi": 31, "urvi": 32,
 
 print(list(sorted(dict.keys())))
 print(list(sorted(dict.values())))
+
+dict1 = {"rajvi": 31, "urvi": 32,
+        "neha": 32, "navisha": 29, "gargi": 34}
+
+print(list(sorted(dict1.keys())))
+print(list(sorted(dict1.values())))
