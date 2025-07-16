2b8812aaa756b06c1b25df84b110dc28677c992e
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-17 00:04:01 +0530

    Added the function for disk usage

--- 2025/monitor_disk_usage_send_notification.py
+++ 2025/monitor_disk_usage_send_notification.py
@@ -6,3 +6,8 @@ MONITORED_PATH = "/"
 def log(msg):
     '''sample timestamped message'''
     print(f"[{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}] {msg}")
+def check_disk_usage(path):
+    """Returns the disk usage as a percentage"""
+    total, used, free = shutil.disk_usage(path)
+    percent_used = used / total * 100
+    return round(percent_used ,2), total, used, free
