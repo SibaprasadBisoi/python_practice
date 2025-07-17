dd4f7fafad64ffc7f71c215a4c2a019e30eef07e
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-17 23:55:09 +0530

    expiry return function added

--- 2025/check_ssl_expiry.py
+++ 2025/check_ssl_expiry.py
@@ -6,3 +6,11 @@ domains = [
     "github.com",
     "dockerhub.io"
 ]
+def get_ssl_expiry(domain, port=443):
+    """Returns the expiry date of ssl certificates"""
+    context = ssl.create_default_context()
+    with socket.create_connection((domain, port), timeout=5)as sock:
+        with context.wrap_socket(sock, server_hostname=domain)as ssock:
+            cert = ssock.getpeercert()
+            expiry = datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
+            return expiry
