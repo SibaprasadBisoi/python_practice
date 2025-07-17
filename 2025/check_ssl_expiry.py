fb99cae5976023d0f4426c12d7ed0f46bd8fbc60
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-18 00:05:07 +0530

    script is completed

--- 2025/check_ssl_expiry.py
+++ 2025/check_ssl_expiry.py
@@ -14,3 +14,12 @@ def get_ssl_expiry(domain, port=443):
             cert = ssock.getpeercert()
             expiry = datetime.strptime(cert['notAfter'], "%b %d %H:%M:%S %Y %Z")
             return expiry
+        
+for domain in domains:
+    try:
+        expiry_date = get_ssl_expiry(domain)
+        days_left = (expiry_date -datetime.utcnow()).days
+        status = "ok" if days_left > 15 else "expiring soon"
+        print(f"{domain:<30} expires in {days_left} days ({expiry_date}) [{status}]")
+    except Exception as e:
+        print(f"{domain:<30} Error: {e}")
