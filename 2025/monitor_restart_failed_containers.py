9b575a88b2e0d1ff10295e3a48305d586d22f205
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-18 23:44:06 +0530

    Function added for getting exited containers

--- 2025/monitor_restart_failed_containers.py
+++ 2025/monitor_restart_failed_containers.py
@@ -1,2 +1,19 @@
 import subprocess
 from datetime import datetime
+def log(msg):
+    """Restart the failed containers"""
+    print(f"[{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}] {msg}")
+
+def get_exited_containers():
+    """Returns list of containers exited 'ids' in exited status"""
+    result = subprocess.run(["docker", "ps", "-a", "--filter", "status = exited", "--format", "{{.ID}} {{.Names}}"], 
+    stdout = subprocess.PIPE,
+    stderr = subprocess.PIPE, 
+    text = True
+    )
+    containers = []
+    for line in result.stdout.strip().split("\n"):
+        if line:
+            container_id, name = line.strip().split()
+            containers.append((container_id, name))
+    return containers
