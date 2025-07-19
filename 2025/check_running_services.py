409ca27b30c1a46effb9f184cab60de3a3f32825
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-19 12:43:56 +0530

    Script is completed

--- 2025/check_running_services.py
+++ 2025/check_running_services.py
@@ -1,5 +1,6 @@
 import subprocess
 from datetime import datetime
+services = ["nginx", "docker", "sshd", "mysql"]
 def log(msg):
     """prints a timestamped message"""
     print(f"[{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}] {msg}")
@@ -14,3 +15,17 @@ def check_service(service):
         )
         status = result.stdout.strip()
         return status
+    except Exception as e:
+        return f"error: {str(e)}"
+if __name__ == "__main__":
+    log("Checking critical services statuses...")
+    for svc in services:
+        status = check_service(svc)
+        if status == "active":
+            log(f"{svc} is running")
+        elif status == "inactive":
+            log(f"{svc} is not running")
+        elif "error" in status:
+            log(f'Error checking {svc}: {status}')
+        else:
+            log(f"Unknown status for {svc}: {status}")
