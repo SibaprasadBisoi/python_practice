3869d1ed6d738a97da1b7c0aaa2f5b9103590788
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-20 18:57:04 +0530

    Added function to find the service status

--- 2025/monitor_windows_services.py
+++ 2025/monitor_windows_services.py
@@ -8,3 +8,20 @@ services = [
 ]
 def log(msg):
     print(f"[{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}] {msg}")
+def check_service_status(service_name):
+    """Returns the status of windows service"""
+    try:
+        result = subprocess.run(
+            ["sc", "qurry", service_name],
+            stdout= subprocess.PIPE,
+            stderr= subprocess.PIPE,
+            text= True
+        )
+        if "RUNNING" in result.stdout:
+            return "RUNNING"
+        elif "STOPPED" in result.stdout:
+            return "STOPPED"
+        else:
+            return "UNKNOWN"
+    except Exception as e:
+        return f"Error: {e}"
