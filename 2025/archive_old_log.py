6116405bfbbde9f93b3221d4d70e55fa115c0e48
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-21 23:49:57 +0530

    Created all the folders

--- 2025/archive_old_log.py
+++ 2025/archive_old_log.py
@@ -2,6 +2,12 @@ import os
 import shutil
 from datetime import datetime
 import time
-logs_directory = r"c:\logs"
-archive_directory = r"c:\archive\logs"
+logs_directory = r"c:/logs"
+archive_directory = r"c:/archive/logs"
 days_old = 7
+os.makedirs(archive_directory, exist_ok=True)
+time_stamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+archive_name= os.path.join(archive_directory, f"archived_logs_{time_stamp}")
+temp_dir = os.path.join(archive_directory, "temp_logs")
+os.makedirs(temp_dir, exist_ok=True)
+
