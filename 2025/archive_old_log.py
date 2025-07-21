654e155d4815dbbb6913bae560e76ce5425fc3ae
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-21 23:57:22 +0530

    Logic added for moving the files to temp dir

--- 2025/archive_old_log.py
+++ 2025/archive_old_log.py
@@ -10,4 +10,12 @@ time_stamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 archive_name= os.path.join(archive_directory, f"archived_logs_{time_stamp}")
 temp_dir = os.path.join(archive_directory, "temp_logs")
 os.makedirs(temp_dir, exist_ok=True)
-
+cut_off = time.time() - (days_old * 86400)
+moved_files= []
+for root,dirs,files in os.walk(logs_directory):
+    for file in files:
+        if file.endswith(".log"):
+            file_path = os.path.join(root, file)
+            if os.path.getmtime(file_path) < cut_off:
+                shutil.move(file_path, (os.path.join(temp_dir, file)))
+                moved_files.append(file)
