3e71d938ad00128e081bbcbcbdb00afec827b74e
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-24 20:45:18 +0530

    Log function is added

--- 2025/append_build_log.py
+++ 2025/append_build_log.py
@@ -3,3 +3,6 @@ from datetime import datetime
 path = "c:/logs"
 os.makedirs(path, exist_ok=True)
 log_path = os.path.join(path, "build.log")
+def log(msg):
+    '''write a message'''
+    print(f"[{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}] {msg}")
