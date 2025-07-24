4fe97d811222906939b33abc98de93914a3b3232
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-24 21:12:23 +0530

    Script is completed

--- 2025/append_build_log.py
+++ 2025/append_build_log.py
@@ -5,4 +5,11 @@ os.makedirs(path, exist_ok=True)
 log_path = os.path.join(path, "build.log")
 def log(msg):
     '''write a message'''
-    print(f"[{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}] {msg}")
+    timestamped_msg = (f"[{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}] {msg}")
+    return timestamped_msg
+build_status = input("enter the build status: ").strip().lower()
+if build_status == "completed":
+    with open(log_path, 'a')as file:
+        file.write(log('Build completed'))        
+else:
+    log(f'Build status is unknown: {build_status}')
