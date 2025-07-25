a643523e7efcd73afa5b024ab5f11a7ba306570a
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-25 23:42:38 +0530

    Script is completed

--- 2025/is_service_running.py
+++ 2025/is_service_running.py
@@ -9,4 +9,15 @@ def log_status(msg):
     timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
     with open(log_file,'a')as file:
         file.write(f"[{timestamp}] {msg}\n")
-        
+def check_service(name):
+    try:
+        result = subprocess.run(["systemctl", "is_active", name], capture_output=True, text=True)
+        status = result.stdout.strip()
+        if status == "is_active":
+            log_status(f'service {name} is runnig')
+        else:
+            log_status(f"service {name} is not running")
+    except Exception as e:
+        log_status(f"error checking the the log {name}")
+if __name__ == "__main__":
+    check_service(service)
