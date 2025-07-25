947374f9228d71deb3b605cab273a5c551fc098e
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-25 23:28:15 +0530

    Log function added

--- 2025/is_service_running.py
+++ 2025/is_service_running.py
@@ -6,3 +6,7 @@ log_dir = './logs'
 os.makedirs(log_dir)
 log_file = os.path.join(log_dir, "service_status.log")
 def log_status(msg):
+    timestamp = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    with open(log_file,'a')as file:
+        file.write(f"[{timestamp}] {msg}\n")
+        
