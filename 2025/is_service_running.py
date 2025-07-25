9d44724852323b2c97b8c0bf5d622e2c081bb480
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-25 23:23:29 +0530

    variables configured

--- 2025/is_service_running.py
+++ 2025/is_service_running.py
@@ -1,3 +1,8 @@
 import os
 import subprocess
 from datetime import datetime
+service = "nginx"
+log_dir = './logs'
+os.makedirs(log_dir)
+log_file = os.path.join(log_dir, "service_status.log")
+def log_status(msg):
