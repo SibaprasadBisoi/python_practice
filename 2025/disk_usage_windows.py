f0ad314ccddef368f543ff8305304499bedc2220
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-26 23:26:37 +0530

    Added logic to find the disk usage in percentage

--- 2025/disk_usage_windows.py
+++ 2025/disk_usage_windows.py
@@ -2,3 +2,7 @@ import os
 import shutil
 target_path = "c://" 
 threshold_percentage = 80
+total, used, free = shutil.disk_usage(target_path)
+print(f"total disk space is: {total} and used is: {used} and free space is {free}")
+used_in_percentage = round((used / total) * 100)
+print(f"Total used space in percentage is: {used_in_percentage}")
