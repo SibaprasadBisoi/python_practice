79270381ea7be8bbf2079b81b083b52e971de4be
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-26 23:59:43 +0530

    Finding the most repeated item in list and the number of times repeated

---
+++ 2025/most_used_char.py
@@ -0,0 +1,10 @@
+string = "abcdavkhlahgacldadanavhkidsacdadhakja;acakj===="
+list = []
+for char in string:
+    list.append(char)
+print(list)
+print(len(list))
+print(max(list))
+most_repeated = max(list, key=list.count)
+count = list.count(most_repeated)
+print(f"The most repeated item is {most_repeated} and {count} times repeated")
