33af69fe9b2dd9d4e2439169452ab350e7863cfe
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-27 07:11:08 +0530

    Script is completed

--- 2025/retain_last_30_logs.py
+++ 2025/retain_last_30_logs.py
@@ -5,3 +5,10 @@ logs_dir = "C:/store/logs"
 retention_count = 30
 log_files = glob.glob(os.path.join("*.log"))
 log_files.sort(key=os.path.getmtime, reverse=True)
+for file in log_files[retention_count:]:
+    try:
+        os.removedirs(file)
+        print(f"Deleted old log file: {file}")
+    except Exception as e:
+        print(f"Error deleting the file {file}: {e}") 
+    
