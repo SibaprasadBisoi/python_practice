2e426e1e65982b303c12a9dfebb589c8d3d98a7d
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-27 07:03:51 +0530

    Sorted files for retention

--- 2025/retain_last_30_logs.py
+++ 2025/retain_last_30_logs.py
@@ -2,3 +2,6 @@ import os
 import glob
 import time
 logs_dir = "C:/store/logs"
+retention_count = 30
+log_files = glob.glob(os.path.join("*.log"))
+log_files.sort(key=os.path.getmtime, reverse=True)
