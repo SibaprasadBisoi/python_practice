f9f6893e3e80c62676265283b971c896fd5b6340
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-07-30 23:33:12 +0530

    Dictionary created

---
+++ 2025/deploy_config_selector.py
@@ -0,0 +1,17 @@
+configs = {
+    "dev": {
+        "region": "us-west-1",
+        "db_url": "dev-db.example.com",
+        "app_version": "1.2.0-dev"
+    },
+    "qa": {
+        "region": "us-east-2",
+        "db_url": "qa-db.example.com",
+        "app_version": "1.2.0-qa"
+    },
+    "prod": {
+        "region": "eu-central-1",
+        "db_url": "prod-db.example.com",
+        "app_version": "1.2.0"
+    }
+}
