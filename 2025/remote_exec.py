d63d6dd789480449f6cb2784e10e88d09f9f62c1
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-02 23:42:05 +0530

    Added script for remote execution

--- 2025/remote_exec.py
+++ 2025/remote_exec.py
@@ -13,4 +13,9 @@ try:
     stdin, stdout, stderr = ssh.exec_command(remote_command)
     output = stdout.read().decode()
     error = stderr.read().decode()
-    
+    if output:
+        print(f"[OUTPUT]\n{output}")
+    if error:
+        print(f"[ERROR]\n{error}")
+except Exception as e:
+    print(f"[Exception]\n str{e}")
