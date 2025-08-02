2bf77798bc243725c341051523d86b959cf6b68a
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-02 23:37:43 +0530

    Added the logic to connect

--- 2025/remote_exec.py
+++ 2025/remote_exec.py
@@ -4,3 +4,13 @@ port = 22
 username = "ubuntu"
 password = "password"
 remote_command = "uptime"
+try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko, autoAddPolicy())
+    print(f"Connecting to {hostname}....")
+    ssh.connect(hostname, port, username, remote_command)
+    print(f"Running remote command: {remote_command}")
+    stdin, stdout, stderr = ssh.exec_command(remote_command)
+    output = stdout.read().decode()
+    error = stderr.read().decode()
+    
