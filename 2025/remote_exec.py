0c8beff2cd586c0d63a78ece64a24f455977d556
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-02 23:43:28 +0530

    Added script for remote execution

--- 2025/remote_exec.py
+++ 2025/remote_exec.py
@@ -6,7 +6,7 @@ password = "password"
 remote_command = "uptime"
 try:
     ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko, autoAddPolicy())
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     print(f"Connecting to {hostname}....")
     ssh.connect(hostname, port, username, remote_command)
     print(f"Running remote command: {remote_command}")
