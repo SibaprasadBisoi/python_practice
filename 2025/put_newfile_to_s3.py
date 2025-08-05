7ff47512f7cf90b7125fd6ec8aefd030e577f69b
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-05 23:36:03 +0530

    Logic added to upload file

--- 2025/put_newfile_to_s3.py
+++ 2025/put_newfile_to_s3.py
@@ -7,3 +7,13 @@ bucket_name = "my-devops-bucket"
 upload_interval = 10
 s3 = boto3.client('s3')
 file_timestamp = {}
+def upload_to_s3(file_path):
+    try:
+        filename = os.path.basename(file_path)
+        s3.upload_file(file_path, bucket_name, filename)
+        print(f"Uploaded {filename}")
+    except FileNotFoundError:
+        print(f"No files found")
+    except NoCredentialsError:
+        print(f"AWS credential not found")
+        
