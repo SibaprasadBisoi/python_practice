0e5e8a4f18f108cf728072b694db2c7b8053802f
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-09 23:20:55 +0530

    logic added to list instances and fetch the name

--- 2025/ec2_instance_list.py
+++ 2025/ec2_instance_list.py
@@ -3,3 +3,13 @@ ec2 = boto3.cient('ec2')
 ec2_list = []
 ec2_dict = {}
 response = ec2.describe_instances()
+for reservation in response['Reservations']:
+    for instance in reservation['instances']:
+        instance_type = instance['InstanceType']
+        name = None
+        if 'Tags' in instance:
+            for tag in instance['Tags']:
+                if tag['key'] == "Name":
+                    name = tag[Value]
+                    break
+                
