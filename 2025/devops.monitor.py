9cf884fd9e838532ae28f036983b3e7e44c816ca
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-14 00:16:34 +0530

    function added to fetch cpu, mem and disk usage

--- 2025/devops.monitor.py
+++ 2025/devops.monitor.py
@@ -7,4 +7,10 @@ def log_message(message):
     with open("log_file", 'r')as f:
         f.write(f"[{time_stamp}] {message}\n")
     print(f"[{time_stamp}] {message}")
-    
+def check_system_usage():
+    cpu_percent = psutil.cpu_percent(interval=1)
+    mem = psutil.virtual_memory()
+    disk = psutil.disk_usage()
+    log_message(f"CPU usage: {cpu_percent}%")
+    log_message(f"Memory usage: {mem.percen}% ({mem.used // (1024**2)}MB / {mem.total // (1024**2)}MB )")
+    log_message(f"Disk usage: {disk.percent}%")
