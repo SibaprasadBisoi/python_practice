3ef81795c37fd3b727232d77e3a8bc58040e99f8
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-14 00:02:19 +0530

    fuction added for logging message

--- 2025/devops.monitor.py
+++ 2025/devops.monitor.py
@@ -2,3 +2,9 @@ import psutil
 import subprocess
 import datetime
 log_file = "C:/store/logs"
+def log_message(message):
+    time_stamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+    with open("log_file", 'r')as f:
+        f.write(f"[{time_stamp}] {message}\n")
+    print(f"[{time_stamp}] {message}")
+    
