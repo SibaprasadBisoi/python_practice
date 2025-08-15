3bab23b9de152458d70c4879558a56f1fce05f60
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-15 23:58:25 +0530

    Checking acl for buckets

--- 2025/s3_bucket_report.py
+++ 2025/s3_bucket_report.py
@@ -7,4 +7,13 @@ for bucket in buckets:
     bucket_name = bucket['Name']
     public_access = False
     details = {"Bucket": bucket_name, "Public": False, "Reason": ""}
-    
+    try:
+        acl = s3.get_bucket_acl(Bucket = bucket_name)
+        for grant in acl["Grants"]:
+            grantee = grant.get("grantee", {})
+            if grantee.get("URI") == "http://acs.amazon.com/groups/global/allusers":
+                public_access = True
+                details["Reason"] = "ACL allows public read/write acces"
+                break
+    except Exception as e:
+        print("acl check failed: {e}")
