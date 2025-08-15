0b24fb3ea5abfb2d9458d2514b4ca3deb2166e49
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-15 23:51:09 +0530

    Fetching the bucket details

--- 2025/s3_bucket_report.py
+++ 2025/s3_bucket_report.py
@@ -3,3 +3,8 @@ import json
 s3 = boto3.client('s3')
 buckets = s3.list_buckets()['Buckets']
 report = []
+for bucket in buckets:
+    bucket_name = bucket['Name']
+    public_access = False
+    details = {"Bucket": bucket_name, "Public": False, "Reason": ""}
+    
