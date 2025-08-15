1a847a2940dac8d2bf640d6e2693d40fdf22529b
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-16 00:17:25 +0530

    script is completed

--- 2025/s3_bucket_report.py
+++ 2025/s3_bucket_report.py
@@ -17,3 +17,26 @@ for bucket in buckets:
                 break
     except Exception as e:
         print("acl check failed: {e}")
+    try:
+        policy = s3.get_bucket_policy(Bucket=bucket_name)
+        policy_json = json.loads(policy["Policy"])
+        for statement in policy_json.get("Statement", []):
+            if statement.get("Effect")=="Allow":
+                principal = statement.get("Principal")
+                if principal == "*" or principal.get("AWS") == "*":
+                    public_access = True
+                    details["Reason"] = "Bucket policy allows public access"
+    except s3.exceptions.NoSuchBucketPolicy:
+        pass
+    except Exception as e:
+        print(f"Error fetching bucket policy: {e}")
+    details["Public"] = public_access
+    report.append(details)
+    with open("s3_security_report.json", 'w') as f:
+        json.dump(report, f, indent=4)
+    print(f"S3 bucket acl and policy report prepared")
+
+
+
+
+
