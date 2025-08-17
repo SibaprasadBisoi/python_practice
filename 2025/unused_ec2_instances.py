201ce4be9da0987463d5aba6eb9d7bef634ef7bd
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-17 23:33:02 +0530

    Logic added for finding stale instances

--- 2025/unused_ec2_instances.py
+++ 2025/unused_ec2_instances.py
@@ -4,3 +4,14 @@ ec2 = boto3.client('ec2')
 cutoff = datetime.now(timezone.utc) - timedelta(days=7)
 reservations = ec2.describe_instances()["Reservations"]
 stale_instances = []
+for reservation in reservations:
+    for instance in reservation['Instances']:
+        state = instance['State']['Name']
+        instance_id = instance['InstanceID']
+        launch_time = instance['LaunchTime']
+        if state == "Stopped" and launch_time < cutoff:
+            stale_instances.append({
+                "InstanceID": instance_id,
+                "State": state,
+                "LauncTime": launch_time
+            })
