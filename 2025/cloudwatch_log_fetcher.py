455977976dc46df1e3b2cb5abb84c449c187350e
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-19 00:45:20 +0530

    Time frame added

--- 2025/cloudwatch_log_fetcher.py
+++ 2025/cloudwatch_log_fetcher.py
@@ -3,3 +3,5 @@ from datetime import datetime, timezone, timedelta
 logs = boto3.client('logs')
 log_group = "aws/lambda/my-function"
 log_stream = "2025/08/16/[$LATEST]abcd1234efgh5678"
+end_time = int(datetime.now(timezone.utc).timestamp() * 1000)
+start_time = int((datetime.now(timezone.utc) - timedelta(minutes=15)).timestamp() * 1000)
