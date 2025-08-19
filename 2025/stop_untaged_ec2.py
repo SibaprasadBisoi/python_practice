9ac3ee099f5427fe559296a753804bdd7d358f53
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-19 23:37:40 +0530

    Logic added to get instance id's

--- 2025/stop_untaged_ec2.py
+++ 2025/stop_untaged_ec2.py
@@ -1,3 +1,14 @@
 import boto3
 ec2 = boto3.client('ec2')
 required_tag = "Owner"
+def get_untagged_instances():
+    instances = []
+    response = ec2.describe_instances()
+
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instace_id = instance['InstanceId']
+            tags = {tag['Key']: tag['Value'] for tag in instance.get("Tags, []")}
+            if required_tag not in tags:
+                instances.append(instace_id)
+                
