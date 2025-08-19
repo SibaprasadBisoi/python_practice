3a460f60ef3ff0a8bacc1b43951b546841f27886
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-19 23:46:09 +0530

    Script is completed

--- 2025/stop_untaged_ec2.py
+++ 2025/stop_untaged_ec2.py
@@ -11,4 +11,14 @@ def get_untagged_instances():
             tags = {tag['Key']: tag['Value'] for tag in instance.get("Tags, []")}
             if required_tag not in tags:
                 instances.append(instace_id)
-                
+    return instances
+def stop_instances(instance_ids):
+    if instance_ids:
+        print(f"Stop instances without {required_tag} tag: {instance_ids}")
+        ec2.stop_insatnces(InstanceIds = instance_ids)
+    else:
+        print(f"All instances have the required tag")
+if __name__ == "__main__":
+    untagged_instances = get_untagged_instances()
+    stop_instances(untagged_instances)
+
