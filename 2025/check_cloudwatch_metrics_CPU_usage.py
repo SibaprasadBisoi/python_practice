08bf888940816c0833a53a133a1b7307e1fd991b
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-22 01:27:51 +0530

    Logic added to fetch instanceid and declred the time period

--- 2025/check_cloudwatch_metrics_CPU_usage.py
+++ 2025/check_cloudwatch_metrics_CPU_usage.py
@@ -2,3 +2,13 @@ import boto3
 from datetime import datetime, timezone, timedelta
 ec2 = boto3.client('ec2')
 cloudwatch = boto3.client('cloudwatch')
+def stop_idle_instances():
+    reservations = ec2.describe_instances(
+        Filters = [{"Name": "Instance-state-name", "Values": ["running"]}]
+    )["Reservations"]
+    for reservation in reservations:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            end_time = datetime.now(timezone.utc)
+            start_time = end_time - timedelta(days= 7)
+            
