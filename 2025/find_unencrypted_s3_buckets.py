aa36575630771dae12fb376555300aa8380d3112
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-22 23:44:34 +0530

    Logic added to find unencrypted s3 buckets

--- 2025/find_unencrypted_s3_buckets.py
+++ 2025/find_unencrypted_s3_buckets.py
@@ -1,5 +1,17 @@
 import boto3
-s3 = boto3.client('s3')
-report = {}
-response = s3.list_buckets()
-buckets = [bucket['Name'] for bucket in response["Buckets"]]
+def check_s3_encryption():
+    s3 = boto3.client('s3')
+    report = {}
+    response = s3.list_buckets()
+    buckets = [bucket['Name'] for bucket in response["Buckets"]]
+    for bucket in buckets:
+        try:
+            enc = s3.get_bucket_encryption(Bucket=bucket)
+            rules = enc['ServerSideEncryptionConfiguration']['Rules']
+            report[bucket] = f"Encrypted ({rules[0]['ApplyServerSideEncryptionByDefault']['SSEAlgorithm']})"
+        except s3.exceptions.ClientError as e:
+            if "ServerSideConfigurationNotFoundError" in str(e):
+                report[bucket] = "Not Encrypted"
+            else:
+                report[bucket] = f"Error checking: {str(e)}"
+
