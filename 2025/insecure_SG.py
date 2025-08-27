d4f61890c008fd1a9c460eb1f6850cfe70e5f95b
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-28 00:18:33 +0530

    Logic added to find out the insecure security groups

--- 2025/insecure_SG.py
+++ 2025/insecure_SG.py
@@ -14,4 +14,15 @@ def find_open_security_groups():
             from_port = perm.get("FromPort")
             to_port = perm.get("ToPort")
 
-            
+            for ip_range in perm.get("IpRanges", []):
+                cidr = ip_range.get("CidrIp")
+                if cidr == "0.0.0.0/0":
+                    insecure_groups.append({
+                        "GroupId": group_name,
+                        "GroupName": group_name,
+                        "FromPort": from_port,
+                        "ToPort": to_port
+                    })
+    return insecure_groups
+
+
