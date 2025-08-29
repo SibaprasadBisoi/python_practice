fe955b9fe6f126aea72fc95bab5194ae9002a2d2
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-29 23:53:36 +0530

    Logic added to check url is up or not

---
+++ 2025/service_check.py
@@ -0,0 +1,12 @@
+import requests
+def service_check(url):
+    try:
+        response = requests.get(url, timeout = 5)
+        if response.status_code == 200:
+            print(f"Service at {url} is up")
+        else:
+            print(f"Service at {url} is not up")
+    except requests.RequestException as e:
+        print(f"Service at {url} is down. Error: {e}")
+        
+        
