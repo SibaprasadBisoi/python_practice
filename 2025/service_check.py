dfa920250b3bbea7dceefa8afd82072b8668c2c8
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-08-29 23:56:23 +0530

    Script is completed

--- 2025/service_check.py
+++ 2025/service_check.py
@@ -8,5 +8,8 @@ def service_check(url):
             print(f"Service at {url} is not up")
     except requests.RequestException as e:
         print(f"Service at {url} is down. Error: {e}")
-        
+if __name__=="__main__":
+    service_url = "https://www.chatgpt.com"
+    service_check(service_url)
+
         
