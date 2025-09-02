a74d65d45c2300d333ff9dbffb75d6f7858f6434
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-09-03 00:27:07 +0530

    Class created for contacts

--- Flask_Project/Day5/main.py
+++ Flask_Project/Day5/main.py
@@ -6,3 +6,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
 db = SQLAlchemy(app)
 
+class Contacts(db.Model):
+    '''sno, name, phone_num, msg, date, email'''
+    sno = db.Column(db.Integer, Primary_key = True)
+    name = db.Column(db.String(80), nullable = False)
+    phone_num = db.Column(db.Integer(12), nullable = False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable =False)
+    email = db.Column(db.String(20), nullable=False)
+    
