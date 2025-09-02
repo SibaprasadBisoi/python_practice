0e797c2eab19b6b34b8608bd88fead238d85e275
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-09-03 00:17:04 +0530

    Passed database configuration

--- Flask_Project/Day5/main.py
+++ Flask_Project/Day5/main.py
@@ -1,3 +1,8 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+db = SQLAlchemy(app)
+
