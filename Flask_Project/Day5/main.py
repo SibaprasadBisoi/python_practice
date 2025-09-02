3eb9cab83fdff26603e4a9e54555a2ce3296905b
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-09-03 00:44:04 +0530

    Program is completed

--- Flask_Project/Day5/main.py
+++ Flask_Project/Day5/main.py
@@ -23,3 +23,17 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route("/contact", methods = ["GET", "POST"])
+def contact():
+    if(request.method == "POST"):
+        '''Add entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+        entry = Contacts(name = name, phone_num = phone, msg = message, date = datetime.now(), email = email)
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('contact.html')
+
+app.run(debug=True)
