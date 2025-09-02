c9bdab783fafc03d6699f6c72c386f81a299f915
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-09-03 00:12:46 +0530

    imported required modules

--- Flask_Project/Day5/main.py
+++ Flask_Project/Day5/main.py
@@ -1,15 +1,3 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return render_template('index.html')
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-@app.route("/contact")
-def contact():
-    return render_template('contact.html')
-
-app.run(debug=True)
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
