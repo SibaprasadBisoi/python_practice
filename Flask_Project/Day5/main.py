bf3bbd8f2114cdc6755c5bba7a253d618a7b89af
SibaprasadBisoi <sibaprasad.7777@gmail.com>
2025-09-03 00:31:29 +0530

    Rendered home and about html

--- Flask_Project/Day5/main.py
+++ Flask_Project/Day5/main.py
@@ -14,4 +14,12 @@ class Contacts(db.Model):
     msg = db.Column(db.String(120), nullable=False)
     date = db.Column(db.String(12), nullable =False)
     email = db.Column(db.String(20), nullable=False)
-    
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
